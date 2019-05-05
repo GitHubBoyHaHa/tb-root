652d605a82200e21ec4ec728f9db005c41bd21f1
pj <pj@newyork.com>
2019-05-05 17:32:39 +0800

    2019-5-5-17:18

--- tb-config-server-8848/src/main/java/com/newyork/ConfigServerApp8848.java
+++ tb-config-server-8848/src/main/java/com/newyork/ConfigServerApp8848.java
@@ -2,13 +2,12 @@ package com.newyork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class UserApp8001 {
-
+@EnableConfigServer
+public class ConfigServerApp8848 {
     public static void main(String[] args) {
-        SpringApplication.run(UserApp8001.class,args);
+        SpringApplication.run(ConfigServerApp8848.class);
     }
 }
